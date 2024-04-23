4b14e32b53e789744e6553911ee2b79cd034ea3e
tanvirtbf <tanver018765@gmail.com>
2024-04-23 11:23:39 +0600

    create findItemIndex function for getting item index

--- Shopping cart Project with Concept/store/slices/cartSlice.js
+++ Shopping cart Project with Concept/store/slices/cartSlice.js
@@ -61,11 +61,16 @@ export default function cartReducer(originalState = [], action) {
   });
 }
 
+const findItemIndex = (state, action) =>   state.findIndex(
+  (cartItem) => cartItem.productId === action.payload.productId
+);
+
 createSlice({
   name: "cart",
   initialState: [],
   reducers: {
     addItem(state, action) {
+      const existingItemIndex = findItemIndex(state, action)
       if (existingItemIndex !== -1) {
         state[existingItemIndex].quantity += 1;
       } else {
@@ -73,12 +78,15 @@ createSlice({
       }
     },
     removeItem(state, action){
+      const existingItemIndex = findItemIndex(state, action)
       state.splice(existingItemIndex, 1);
     },
     increaseItemQuantity(state, action){
+      const existingItemIndex = findItemIndex(state, action)
       state[existingItemIndex].quantity += 1;
     },
     decreaseItemQuantity(state, action){
+      const existingItemIndex = findItemIndex(state, action)
       state[existingItemIndex].quantity -= 1;
       if (state[existingItemIndex].quantity === 0) {
         state.splice(existingItemIndex, 1);
