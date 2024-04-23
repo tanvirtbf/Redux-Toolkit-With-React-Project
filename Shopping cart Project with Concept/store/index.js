4a55341a6be6418291917501ef7aa51b2fc4a747
tanvirtbf <tanver018765@gmail.com>
2024-04-23 12:22:13 +0600

    remove combineReducer and wrapped in configureStore function

--- Shopping cart Project with Concept/store/index.js
+++ Shopping cart Project with Concept/store/index.js
@@ -4,10 +4,11 @@ import cartReducer from "./slices/cartSlice";
 import productsReducer from "./slices/productsSlice";
 import wishListReducer from "./slices/wishListSlice";
 
-const reducer = combineReducers({
-  products: productsReducer,
-  cartItems: cartReducer,
-  wishList: wishListReducer,
-});
 
-export const store = configureStore({ reducer: reducer });
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    cartItems: cartReducer,
+    wishList: wishListReducer,
+  },
+});
