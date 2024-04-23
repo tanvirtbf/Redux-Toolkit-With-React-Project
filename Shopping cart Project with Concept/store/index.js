4b80249d9e64ff7c89553ba88bb1a5c1ccef64e4
tanvirtbf <tanver018765@gmail.com>
2024-04-23 12:23:23 +0600

    uninstall redux library

--- Shopping cart Project with Concept/store/index.js
+++ Shopping cart Project with Concept/store/index.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
 import cartReducer from "./slices/cartSlice";
 import productsReducer from "./slices/productsSlice";
 import wishListReducer from "./slices/wishListSlice";
