1134ba00e57e3acf0680baa3cfcc0f2e8de06c25
tanvirtbf <tanver018765@gmail.com>
2024-04-23 12:20:23 +0600

    connect configure store and remove createStore function

--- Shopping cart Project with Concept/store/index.js
+++ Shopping cart Project with Concept/store/index.js
@@ -1,30 +1,13 @@
-import { combineReducers, createStore } from 'redux'
-import productsReducer from './slices/productsSlice'
-import cartReducer from './slices/cartSlice'
-import wishListReducer from './slices/wishListSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import cartReducer from "./slices/cartSlice";
+import productsReducer from "./slices/productsSlice";
+import wishListReducer from "./slices/wishListSlice";
 
 const reducer = combineReducers({
   products: productsReducer,
   cartItems: cartReducer,
   wishList: wishListReducer,
-})
+});
 
-export const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__?.()
-)
-
-const users = [
-  {
-    name: 'Anurag',
-    age: 26,
-  },
-  {
-    name: 'Ram',
-    age: 18,
-  },
-  {
-    name: 'Adarsh',
-    age: 16,
-  },
-]
+export const store = configureStore({ reducer: reducer });
