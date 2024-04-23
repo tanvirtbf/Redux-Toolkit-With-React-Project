bb0ee37ef93f7592c4e54958bfca19f7fde8b6ed
tanvirtbf <tanver018765@gmail.com>
2024-04-23 14:51:47 +0600

    add middleware in index.js

--- Shopping cart Project with Concept/store/index.js
+++ Shopping cart Project with Concept/store/index.js
@@ -1,8 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Tuple } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
 import productsReducer from "./slices/productsSlice";
 import wishListReducer from "./slices/wishListSlice";
 
+function logger(store) {
+  return function (next) {
+    return function (action) {
+      console.log('store : ', store);
+      console.log('next : ', next);
+      console.log('action : ', action);
+    };
+  };
+}
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +19,5 @@ export const store = configureStore({
     cartItems: cartReducer,
     wishList: wishListReducer,
   },
-  middleware: []
+  middleware: () => new Tuple(logger)
 });
